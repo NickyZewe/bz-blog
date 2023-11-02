4d2de347a8d21f04ef3f44acf1305bb36556fd5e
NickyZewe <thenzewe@yahoo.com>
2023-11-02 16:07:12 -0400

    commit

--- entities/pocketBase.ts
+++ entities/pocketBase.ts
@@ -1,5 +1,5 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase("http://127.0.0.1:8090");
+export const pb = new PocketBase("https://bz-blog.fly.dev/");
 
-export const pocketBaseURL = `http://127.0.0.1:8090`;
+export const pocketBaseURL = `https://bz-blog.fly.dev/`;
